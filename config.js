140a50c8d4cff8fd1f3ba572558a5303fe3a4ced
Joker165 <145442092+Joker165@users.noreply.github.com>
2024-12-29 18:47:43 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  BOT_TOKEN: "8179880664:AAEgNF1pa5aRJDaC6NctS8Vy_C6QjNd_3YA",
-  OWNER_ID: "7352832303",
+  BOT_TOKEN: "7828323771:AAFkK-ZIb0WeT-Wi63Muze2-kayuFxuxTZA",
+  OWNER_ID: "6516704660",
   allowedGroupIds: [-4688957266],
 };
