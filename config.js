51c6381ce1790bffeca4c1343d749c6d2160017c
Joker165 <145442092+Joker165@users.noreply.github.com>
2024-12-31 00:28:38 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // BOT TELE
 module.exports = {
-  BOT_TOKEN: "", // Token bot Telegram
-  OWNER_ID: "", // ID pemilik bot
+  BOT_TOKEN: "7637828567:AAGBTyFgQllXkPzq2yDQ9Z85_sP9UMA82Ds", // Token bot Telegram
+  OWNER_ID: "6516704660", // ID pemilik bot
   allowedGroupIds: [-1002401306074, -1002361556670, -1002386380830, -1002415460020, -4514397992], // ID grup yang diizinkan
 };
