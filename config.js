5a61c9dd0e9c195ae86611e7ab9028f066efb3c5
Joker165 <145442092+Joker165@users.noreply.github.com>
2024-12-31 00:24:59 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
+// BOT TELE
 module.exports = {
-  BOT_TOKEN: "7596021271:AAGBiTrdyULgEps7MFSHe8TwRBB3KnCEhQs",
-  OWNER_ID: "6516704660",
-  allowedGroupIds: [-4688957266],
+  BOT_TOKEN: "", // Token bot Telegram
+  OWNER_ID: "", // ID pemilik bot
+  allowedGroupIds: [-1002401306074, -1002361556670, -1002386380830, -1002415460020, -4514397992], // ID grup yang diizinkan
 };
