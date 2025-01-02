8cce6be55d794fe7b0396b9350d31ae48db2827b
Joker165 <145442092+Joker165@users.noreply.github.com>
2025-01-02 16:54:45 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  BOT_TOKEN: "7654530924:AAFS3Paxh85I7LqbDArppC9F_h7hL8TtvKs", // Token bot Telegram
-  OWNER_ID: "7055787935", // ID pemilik bot
+  BOT_TOKEN: "7596021271:AAGBiTrdyULgEps7MFSHe8TwRBB3KnCEhQs", // Token bot Telegram
+  OWNER_ID: "6516704660", // ID pemilik bot
   allowedGroupIds: [-1002401306074, -1002361556670, -1002386380830, -1002415460020, -4514397992], // ID grup yang diizinkan
 };
