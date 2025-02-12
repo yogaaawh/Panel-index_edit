bda9cbf2f5080d5b61c344b37011ca046a214a90
Joker165 <145442092+Joker165@users.noreply.github.com>
2025-02-12 13:15:06 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-//By dev
 module.exports = {
-  BOT_TOKEN: "7596021271:AAGBiTrdyULgEps7MFSHe8TwRBB3KnCEhQs",
-    allowedDevelopers: ['6516704660'], // ID
+  BOT_TOKEN: "7862652803:AAFBvirVxLzMiEw9flgi1jBQXuzitlfRkLE",
 };
