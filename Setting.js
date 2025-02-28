4a268174e46aa03457f511628e645be5e6c46290
Joker165 <145442092+Joker165@users.noreply.github.com>
2025-02-28 18:05:22 +0100

    Add files via upload

---
+++ Setting.js
@@ -0,0 +1,10 @@
+/*
+         Created by Zephyrine 
+        Telegram : @rainoneday
+   All Right Reversed By Zephyrine
+*/
+
+module.exports = {
+    TokenS: "TOKEN_BOT_MU",
+    admins: ["7308812889"]
+};
